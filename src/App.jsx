dddedd21c408b8da7cf93f3bc2a051cfe1fd7d51
Naveen Verma <nverma2k@gmail.com>
2025-09-19 20:57:10 +0530

    Commit 5: Making components reusable using props.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import headerImage from './assets/react-core-concepts.png'
+import headerImg from './assets/react-core-concepts.png'
+import { CORE_CONCEPTS } from './data';
 
 const reactDescription = ['Fundamental', 'Crucial', 'Core'];
 
@@ -11,7 +12,7 @@ function Header() {
 
   return (
   <header>
-    <img src={headerImage} alt="Stylized atom" />
+    <img src={headerImg} alt="Stylized atom" />
     <h1>React Essentials</h1>
     <p>
       {description} React concepts you will need for almost any app you are going
@@ -21,12 +22,34 @@ function Header() {
   );
 }
 
+function CoreConcepts({image, title, description}) {
+  return (
+    <li>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  );
+}
+
 function App() {
   return (
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concept</h2>
+          <ul>
+            <CoreConcepts 
+              title = {CORE_CONCEPTS[0].title}
+              description = {CORE_CONCEPTS[0].description}
+              image = {CORE_CONCEPTS[0].image}
+            />
+            <CoreConcepts {...CORE_CONCEPTS[1]}/>
+            <CoreConcepts {...CORE_CONCEPTS[2]}/>
+            <CoreConcepts {...CORE_CONCEPTS[3]}/>
+          </ul>
+        </section>
       </main>
     </div>
   );
