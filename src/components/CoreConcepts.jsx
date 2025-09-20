c152a8dbf2b7378ecd73ea2564c46b071e71c64d
Naveen Verma <nverma2k@gmail.com>
2025-09-20 19:08:21 +0530

    Commit 16: Splitting components by feature and state.

--- src/components/CoreConcepts.jsx
+++ src/components/CoreConcepts.jsx
@@ -1,9 +1,23 @@
-export default function CoreConcepts({image, title, description}) {
+import CoreConcept from "./CoreConcept";
+import { CORE_CONCEPTS } from "../data";
+
+export default function CoreConcepts() {
   return (
-    <li>
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
+    <section id="core-concepts">
+      <h2>Core Concept</h2>
+      <ul>
+        {CORE_CONCEPTS.map((conceptItem) => (
+          <CoreConcept key={conceptItem.title} {...conceptItem} />
+        ))}
+        {/* <CoreConcept
+                title={CORE_CONCEPTS[0].title}
+                description={CORE_CONCEPTS[0].description}
+                image={CORE_CONCEPTS[0].image}
+        />
+        <CoreConcept {...CORE_CONCEPTS[1]} />
+        <CoreConcept {...CORE_CONCEPTS[2]} />
+        <CoreConcept {...CORE_CONCEPTS[3]} />  */}
+      </ul>
+    </section>
   );
 }
