050ac9351968d30d3fb6603df3e302388a0241fe
Naveen Verma <nverma2k@gmail.com>
2025-09-20 10:38:28 +0530

    Commit 10: Deriving and output data based on state.

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,15 @@ import Header from "./components/Header/Header";
 import { CORE_CONCEPTS } from "./data";
 import CoreConcepts from "./components/CoreConcepts";
 import TabButton from "./components/TabButton";
+import { EXAMPLES } from "./data"
 
 function App() {
 
-  const [ selectedTopic,setSeletedTopic ] = useState('Please click a button');
+  const [ selectedTopic,setSeletedTopic ] = useState('components');
 
   function handleSelect(selectedButton) {
     setSeletedTopic(selectedButton);
-    console.log(selectedTopic);
+    // console.log(selectedTopic);
   }
 
   return (
@@ -34,12 +35,18 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={() => handleSelect('component')}>Component</TabButton>
+            <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
             <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
-            <TabButton onSelect={() => handleSelect('prop')}>Prop</TabButton>
+            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-          {selectedTopic}
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>{EXAMPLES[selectedTopic].code}</code>
+            </pre>
+          </div>
         </section>
       </main>
     </div>
